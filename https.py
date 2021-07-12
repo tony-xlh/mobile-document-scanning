0ba7ef4dbbbde186935a5c332e84febb2a479867
xulihang <xulihanghai@163.com>
2021-07-12 11:29:14 +0800

    Remove unneccessary files

--- https.py
+++ https.py
@@ -2,7 +2,7 @@
 #python version 3.7
 import http.server, ssl
 
-server_address = ('0.0.0.0', 8088)
+server_address = ('0.0.0.0', 8000)
 httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(httpd.socket,
                                server_side=True,
